c1b6e8be2e95bca584e6679ee8427740b71763c8
Dima <dimarkens@gmail.com>
2022-03-16 06:08:22 -0700

    Update urls.py

--- incora_project/api/urls.py
+++ incora_project/api/urls.py
@@ -6,9 +6,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
-    # path('auth/', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.jwt')),
-
     path('users/', UserView.as_view()),
     path('login/', UserLoginView.as_view()),
     path('users/<int:pk>/', UserDetailView.as_view()),
